325b5c44909fd219a84050f843af8875163562a2
Luke Armstrong <me@lukearmstrong.co.uk>
2017-01-29 15:27:11 +0000

    setup asteroid

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+	void OnTriggerEnter(Collider other)
+	{
+//		if (other.CompareTag("Boundary")) {
+		if (other.tag == "Boundary") {
+			// Ignore Boundary
+			return;
+		}
+
+		Destroy(other.gameObject); // Bolt
+		Destroy(gameObject); // Asteroid
+	}
+}
